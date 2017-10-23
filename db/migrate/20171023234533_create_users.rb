dec61deb75c18f01b4fb6f4ec2d1216e5a1f64a3
rizhima <arishermawan@hotmail.com>
2017-10-24 06:53:13 +0700

    make new model & database create + migrate

---
+++ db/migrate/20171023234533_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :full_name
+
+      t.timestamps
+    end
+  end
+end
