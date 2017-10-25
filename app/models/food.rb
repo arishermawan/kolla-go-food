7be00f69412b4151140b3c4cb17903f5dcb2754a
rizhima <arishermawan@hotmail.com>
2017-10-25 10:14:44 +0700

    add rspec complete for food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,9 @@
 class Food < ApplicationRecord
   validates :name, :description, presence: true
-  validates :image_url 
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :name, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png|)\z}i,
+    message: 'must be a URL for GIF, JPG, or PNG Image.'
+  }
 end
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -20,6 +20,31 @@ describe Food do
     expect(food.errors[:name]).to include("can't be blank")
   end
 
-  it "is invalid without a description"
-  it "is invalid with a duplicate name"
+  it "is invalid without a description" do
+    food = Food.new(
+      name: "Nasi Uduk",
+      description: nil,
+      price: 10000.0
+    )
+    food.valid?
+    expect(food.errors[:description]).to include("can't be blank")
+  end
+
+  it "is invalid with a duplicate name" do
+    food1 = Food.create(
+      name: "Nasi Uduk",
+      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+      price: 10000.0
+    )
+
+    food2 = Food.new(
+      name: "Nasi Uduk",
+      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+      price: 10000.0
+    )
+
+    food2.valid?
+    expect(food2.errors[:name]).to include("has already been taken")
+  end
+  
 end
