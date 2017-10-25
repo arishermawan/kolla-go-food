061a991c19312a42440eef9edf4d2717366f9c94
rizhima <arishermawan@hotmail.com>
2017-10-25 10:32:30 +0700

    add rspec test for Class Method

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -46,5 +46,48 @@ describe Food do
     food2.valid?
     expect(food2.errors[:name]).to include("has already been taken")
   end
-  
+
+  it "returns a sorted array of results that match" do
+    food1 = Food.create(
+      name: "Nasi Uduk",
+      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+      price: 10000.0
+    )
+
+    food2 = Food.create(
+      name: "Kerak Telor",
+      description: "Betawi style traditonal spicy ommelete wit glutinous rice cooker",
+      price: 8000.0
+    )
+
+    food3 = Food.create(
+      name: "Nasi Semur Jengkol",
+      description: "Based on dongfruit, this menu promises a delicious taste",
+      price: 8000.0
+    )
+    expect(Food.by_letter("N")).to eq([food3, food1])
+  end
+
+  it "omits results that do not match" do
+    food1 = Food.create(
+      name: "Nasi Uduk",
+      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+      price: 10000.0
+    )
+
+    food2 = Food.create(
+      name: "Kerak Telor",
+      description: "Betawi style traditonal spicy ommelete wit glutinous rice cooker",
+      price: 8000.0
+    )
+
+    food3 = Food.create(
+      name: "Nasi Semur Jengkol",
+      description: "Based on dongfruit, this menu promises a delicious taste",
+      price: 8000.0
+    )
+    expect(Food.by_letter("N")).not_to eq(food2)
+  end
+
+
 end
