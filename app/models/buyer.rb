02b49c1737b4af23b06298d88477a287e276aeb6
rizhima <arishermawan@hotmail.com>
2017-10-25 09:34:19 +0700

    buyer validation

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,2 +1,6 @@
 class Buyer < ApplicationRecord
+  validates :email, :name, :phone, :address, presence: true
+  validates :email, uniqueness: true
+  validates :phone, numericality: { only_integer: true }, length: { minimum: 5, maximum:12 }
+  validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 end
