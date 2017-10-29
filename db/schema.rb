7bedca40aefc3873632445c7ed0d0e270ebeb713
rizhima <arishermawan@hotmail.com>
2017-10-29 17:24:40 +0700

    create category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171027070934) do
+ActiveRecord::Schema.define(version: 20171029101327) do
 
   create_table "buyers", force: :cascade do |t|
     t.string "email"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20171027070934) do
   end
 
   create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
