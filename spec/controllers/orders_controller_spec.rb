fcba23dd43eabcdcc7034eb2ec55b04ba7bf74d6
rizhima <arishermawan@hotmail.com>
2017-11-01 15:34:06 +0700

    add orders controller speck

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -2,105 +2,175 @@ require 'rails_helper'
 
 
 describe OrdersController do
-  describe 'POST #create' do
-    before :each do
-      @food = create(:food)
+  it "includes CurrentCart" do
+    expect(OrdersController.ancestors.include?CurrentCart).to eq(true)
+  end
+
+  describe 'GET #index' do
+    it "populates an array of all orders" do
+      order1  = create(:order, name: "Buyer 1")
+      order2  = create(:order, name: "Buyer 2")
+      get :index
+      expect(assigns(:orders)).to match_arrary([order1, order2])
     end
 
-    it "Includes CurrentCart" do
-      expect(LineItemsController.ancestors.include? CurrentCart).to eq(true)
+    it "renders te :index tmplate" do  
+      get :index
+      expect(response).to render_template :index
+    end   
+  end
+
+  describe 'GET #show' do
+    it "assigns the requested order to @order" do
+      order = create(:order)
+      get :show, params:{ id: order }
+      expect(assigns(:order)).to eq order
     end
 
-    context "with existing cart" do 
-      it "does not create a new cart before saving a new line_item" do
-        cart = create(:cart)
-        session[:cart_id] = cart.id
+    it "renders the :index tmplate" do  
+      get :show, params:{ id: order }
+      expect(response).to render_template :show
+    end     
+  end
 
-        expect{
-          post :create, params: {food_id: @food.id}
-        }.not_to change(Cart, :count)
-
-      end
-    end 
-
-    context "without existing cart" do 
-      it "creates a new cart before saving a new line_item" do
-        expect{
-          post :create, params: {food_id: @food.id}
-        }.to change(Cart, :count).by(1)
-      end
-    end
-
-    it "saves the new line_item in the database" do
-      expect{
-        post :create, params: {food_id: @food.id}
-      }.to change(LineItem, :count).by(1)
-    end
-    it "redirect to store inex" do
-      post :create, params: { food_id: @food.id }
-      # expect(response).to redirect_to(cart_path(assigns(:line_item).cart))
-      expect(response).to redirect_to (store_index_path)
-    end
-
-    context "with existing line_item with the same food" do
+  describe 'GET #new' do
+    
+    context " with valid cart " do
       before :each do
-        cart = create(:cart)
-        session[:cart_id] = cart.id
-        line_item = create(:line_item, food: @food, cart: cart)
+        @cart = create(:cart)
+        session[:cart_id] = @cart_id
+        @line_item = create(:line_item, cart:@cart)
       end
 
-      it "does not save the new line_item in the database" do
-        expect{
-          post :create, params: { food_id: @food.id }
-        }.not_to change(LineItem, :count)
+      it "assigns a new order to @order" do
+        get :new
+        expect(assigns(:order)).to be_a_new(Order)
       end
 
-      it "increments the quantity of line_item with the same food" do
-        post :create, params: { food_id: @food.id }
-        expect(assigns(:line_item).quantity).to eq(2)
-      end
-    end
-
-    context "without existing line_item with the same food" do
-      it "saves the new line_item in the database" do
-        expect{
-          post :create, params: { food_id: @food.id }
-        }.to change(LineItem, :count).by(1)
+      it "renders the new template" do
+        get :new
+        expect(response).to render_template :new
       end
     end
 
-    # context "without existing line_item with the same food" do
-    #   it "saves the new line_item in the database" do
-    #     expect{
-    #       post :create, params: { food_id: @food.id }
-    #     }.to change(LineItem, :count).by(1)
-    #   end
-    # end
+    context " with empty cart " do
+      before :each do
+        @cart = create(:cart)
+        session[:cart_id] = @cart_id
+      end
 
-    # context "with existing food" do
-    #   it "does add a new line_item in the database" do
-    #     cart = create(:cart)
-    #     line_item =  create(:line_item, food:@food, cart:cart)
+      it "redirect to store index" do
+        get :new
+        expect(response).to redirect_to store_index_url
+      end
+    end
 
-    #     expect{
-    #       post :create, params: {food_id: @food.id}
-    #     }.not_to change(LineItem, count)
-    #   end
+  end
 
-    #   it "increment the quantity of line_item with the same food" do
-    #     cart = create(:cart)
-    #     line_item =  create(:line_item, food:@food, cart:cart)
-    #     post :create, params: {food_id: @food.id}
-    #     expect(line_item.quantity).to eq(2)
-    #   end
-    # end
+  describe 'GET #edit' do
+    it "assigns the requested order to @order" do
+      order = create(:order)
+      get :edit, params:{ id: order }
+      expect(assigns(:order)).to eq order
+    end
 
-    # context "without existing food" do
-    #   it "saves the new line_item in the database" do
-    #     expect{
-    #       post :create, params: {food_id: @food.id}
-    #     }.not_to change(LineItem, :count).by(1)
-    #   end
+    it "renders the :edit tmplate" do  
+      get :show, params:{ id: order }
+      expect(response).to render_template :edit
+    end     
+  end
 
+  describe 'GET #create' do
+    context "with valid attributes"
+      it "save the new order in the database" do
+        expect{
+        post :create, params:{ order: attributes_for(:order) }
+        }.to change(Order, :count).by(1)
+      end
+
+      it "destroy session's cart " do  
+        expect{
+        post :create, params:{ order: attributes_for(:order) }
+        }.to change(Cart, :count).by(-1)
+      end
+
+      it "removes th cart from session's params " do  
+        post :create, params:{ order: attributes_for(:order) }
+        expect(session[:cart_id]).to eq(nil)
+      end
+
+      it "redirect to store index" do
+        post :create, params:{ order: attributes_for(:order)
+        expect(response).to redirect_to store_index_url
+      end
+    end
+
+    context "with invalid attributes"
+      it "does not save the new order in the database " do  
+        expect{
+          post :create, params:{ order: attributes_for(:invalid_order) }
+        }.not_to change(Order, :count)
+      end
+
+      it "rerenders the :new template" do
+        post :create, params:{ order: attributes_for(:invalid_order)
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+  describe 'GET #update' do
+    
+    before :each do
+      @order = create(:order)
+    end
+
+    context "with valid attributes"
+      it "locates the requested @order" do
+        patch :update, params:{ id: @order, order: attributes_for(:order) }
+        expect(assigns(:order)).to eq @order
+      end
+
+      it "changes @order's attributes " do  
+        patch :update, params:{ id: @order, order: attributes_for(:order, name:"nasi uduk") }
+        order.reload
+        expect(@order.name).to eq('nasi uduk')
+      end
+
+      it "redirect to the order" do
+        patch :update, params:{ id: @order, order: attributes_for(:order) }
+        expect(response).to redirect_to @order
+      end
+    end
+
+    context "with invalid attributes"
+      it "does not update the order in the database " do  
+        patch :update, params:{ id: @order, order: attributes_for(:order, name:"nasi uduk", address: nil) }
+        order.reload
+        expect(@order.name).not_to eq('nasi uduk')
+      end
+
+      it "rerenders the :edit template" do
+        patch :update, params:{ id: @order, order: attributes_for(:invalid_order) }
+        expect(response).to render_template :edit
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    before :each do
+      @order = create(:order)
+    end
+
+    it "deletes the order from the database" do
+      expect{
+      delete :destroy, params:{ id: @order }
+      }.to change(Order, :count).by(-1)
+    end
+
+    it "redirect to order#index" do  
+      delete :destroy, params:{ id: order }
+      expect(response).to redirect_to orders_url
+    end     
   end
 end
