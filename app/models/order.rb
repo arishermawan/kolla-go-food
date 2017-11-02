c0c37d65dfaa924470f117135b35809262b83d2b
rizhima <arishermawan@hotmail.com>
2017-11-02 13:16:10 +0700

    add view index & order details

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,16 +6,20 @@ class Order < ApplicationRecord
     "Go Pay" => 1,
     "Credit Card" => 2
   }
-  
+
   validates :name, :address, :email, :payment_type, presence: true
   validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
   validates :payment_type, inclusion: payment_types.keys
 
   def add_line_items(cart)
-    cart.line_items.each do |item|  
+    cart.line_items.each do |item|
       item.cart_id = nil
       line_items << item
     end
+  end
+
+  def total_price
+    line_items.reduce(0) { |sum, line_item| sum+line_item.total_price }
   end
 
 
