4441596c52ce6c2d98156dbb1dd6f44cbc54c886
rizhima <arishermawan@hotmail.com>
2017-11-13 16:41:59 +0700

    add gopay validation to user model [aris]

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
 
   validates :username, presence: true, uniqueness:true
+  validates :gopay, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
   validates :password, presence: true, on: :create
   validates :password, length: { minimum: 8 }, allow_blank:true
 end
